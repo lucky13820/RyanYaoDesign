17c05b1ecaf0b29d784767b47e7146518502f93b
Ryan Yao <ryan.yao@richwaytech.caryan.yao@richwaytech.ca>
2019-09-12 17:12:38 -0700

    new content

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,94 +2,97 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
 
-  // This option controls if and how source maps are generated.
-  // https://webpack.js.org/configuration/devtool/
-  devtool: 'eval-cheap-module-source-map',
+    // This option controls if and how source maps are generated.
+    // https://webpack.js.org/configuration/devtool/
+    devtool: 'eval-cheap-module-source-map',
 
-  // https://webpack.js.org/concepts/entry-points/#multi-page-application
-  entry: {
-    main: './src/main.js',
-  },
+    // https://webpack.js.org/concepts/entry-points/#multi-page-application
+    entry: {
+        main: './src/main.js',
+    },
 
-  // https://webpack.js.org/configuration/dev-server/
-  devServer: {
-    port: 7077,
-    writeToDisk: false // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
-  },
+    // https://webpack.js.org/configuration/dev-server/
+    devServer: {
+        port: 7077,
+        writeToDisk: false // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
+    },
 
-  // https://webpack.js.org/concepts/loaders/
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
-        }
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader'
-          // Please note we are not running postcss here
-        ]
-      },      
-      {
-        test: /\.(gif|png|jpe?g|svg|ico)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'file-loader',
-            options: {
-              publicPath: '/',
-              name: '[name].[ext]',
-              context: 'src'
+    // https://webpack.js.org/concepts/loaders/
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
             },
-          },
-        ],
-      
-      },
-      {
-        test: /\.woff(2)?$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10000,
-              publicPath: '/',
-              name: './fonts/[path][name].[ext]',
-              mimetype: 'application/font-woff'
-            }
-          }
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader'
+                    // Please note we are not running postcss here
+                ]
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg|ico)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            publicPath: '/',
+                            name: '[name].[ext]',
+                            context: 'src'
+                        },
+                    },
+                ],
+
+            },
+            {
+                test: /\.woff(2)?$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        publicPath: '/',
+                        name: './fonts/[path][name].[ext]',
+                        mimetype: 'application/font-woff'
+                    }
+                }]
+            },
+            { test: /\.xml$/, loader: 'xml-loader' },
+
         ]
-      },
-      { test: /\.xml$/, loader: 'xml-loader' },
+    },
 
+    // https://webpack.js.org/concepts/plugins/
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: 'body',
+            chunks: ['main', 'index'],
+            filename: 'index.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/classtopbase.html',
+            inject: 'body',
+            chunks: ['main', 'classtopbase'],
+            filename: 'classtopbase.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/classtopbase-website.html',
+            inject: 'body',
+            chunks: ['main', 'classtopbase-website'],
+            filename: 'classtopbase-website.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/air-app.html',
+            inject: 'body',
+            chunks: ['main', 'air-app'],
+            filename: 'air-app.html'
+        }),
     ]
-  },
-
-  // https://webpack.js.org/concepts/plugins/
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: 'body',
-      chunks: ['main', 'index'],
-      filename: 'index.html'
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/classtopbase.html',
-      inject: 'body',
-      chunks: ['main', 'classtopbase'],
-      filename: 'classtopbase.html'
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/classtopbase-website.html',
-      inject: 'body',
-      chunks: ['main', 'classtopbase-website'],
-      filename: 'classtopbase-website.html'
-    }),
-  ]
 }
