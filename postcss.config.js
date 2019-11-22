b1a9e0bccbacb28458ca88f0b11e0659172e50c4
Ryan Yao <ryanqyao@gmail.com>
2019-11-22 15:01:42 -0800

    small changes

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,13 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-
-  // Specify the paths to all of the template files in your project 
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
   content: [
-    './src/*.html',
+    "*.html"
     // etc.
   ],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-})
+  defaultExtractor: content => content.match(/[\w-/:]*[\w-/:]/g) || []
+});
 module.exports = {
   plugins: [
     // ...
